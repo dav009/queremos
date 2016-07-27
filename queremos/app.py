db229c9037b3670ad3af0ea5f7adbd719b1e847b
David Przybilla <dav.alejandro@gmail.com>
2016-07-27 17:19:00 +0700

    exporting pdfs via weasyprint

--- queremos/app.py
+++ queremos/app.py
@@ -1,7 +1,7 @@
 from queremos import create_mock_letter
 from flask import current_app, Flask, request, Response, render_template, Markup
 import mimerender
-from weasyprint import HTML
+from weasyprint import HTML, CSS
 
 mimerender.register_mime('pdf', ('application/pdf',))
 mimerender = mimerender.FlaskMimeRender(global_charset='UTF-8')
@@ -9,14 +9,14 @@ mimerender = mimerender.FlaskMimeRender(global_charset='UTF-8')
 app = Flask(__name__)
 
 def render_pdf(html):
-    pdf = HTML(string=html).write_pdf()
+    pdf = HTML(string=html).write_pdf(stylesheets=[CSS(string='@page { size: A4; margin: 1cm }')])
     return pdf
 
 @app.route('/generar_solicitar', methods=['GET'])
 @mimerender(default='pdf', pdf=render_pdf)
 def generar():
     html = render_template('solicitud.html', content=Markup(create_mock_letter()))
-    return { 'html': html } # mimerender requires a dict
+    return { 'html': html }
 
 
 if __name__ == '__main__':
