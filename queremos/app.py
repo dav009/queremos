66f00aad8f5ce4a7482350891a4e91941c6890e8
David Przybilla <dav.alejandro@gmail.com>
2016-07-29 18:17:19 +0700

    handling form on server side

--- queremos/app.py
+++ queremos/app.py
@@ -1,7 +1,8 @@
-from queremos import create_mock_letter
+from queremos import create_mock_letter, generate_base_letter
 from flask import current_app, Flask, request, Response, render_template, Markup
 import mimerender
 from weasyprint import HTML, CSS
+import json
 
 mimerender.register_mime('pdf', ('application/pdf',))
 mimerender = mimerender.FlaskMimeRender(global_charset='UTF-8')
@@ -13,10 +14,78 @@ def render_pdf(html):
     pdf = HTML(string=html).write_pdf(stylesheets=page_settings)
     return pdf
 
-@app.route('/generar_solicitar', methods=['GET'])
+
+
+@app.route('/generar_solicitar', methods=['POST'])
 @mimerender(default='pdf', pdf=render_pdf)
 def generar_solicitud():
-    html = render_template('solicitud.html', solicitud=create_mock_letter())
+    answers = {}
+    if "json" in request.content_type:
+        answers = request.json
+    elif request.form:
+        try:
+            serialized_form = request.form
+            print(list(serialized_form.keys()))
+            
+            answers = {
+               "fecha": "today",
+
+               "solicitante": {
+                 "nombre": serialized_form['solicitante_nombre'],
+                 "apellido1": serialized_form['solicitante_apellido1'],
+                 "apellido2": serialized_form['solicitante_apellido2'],
+                 "tipo_identificacion": serialized_form['solicitante_tipo_id'],
+                 "identificacion": serialized_form['solicitante_identificacion'],
+                 "email":serialized_form['solicitante_email'],
+                 "telefono": serialized_form['solicitante_telefono'],
+                 "direccion": serialized_form['solicitante_direccion']
+               },
+
+               "institucion": {
+                 "nombre": serialized_form['entidad_nombre'],
+                 "ciudad": serialized_form['entidad_ciudad'],
+                 "direccion": serialized_form['entidad_direccion'],
+                 "telefono": serialized_form['entidad_telefono']
+               },
+
+               "funcionario": {
+                 "nombre": serialized_form['funcionario_nombre'],
+                 "apellido1": serialized_form['funcionario_apellido1'],
+                 "apellido2": serialized_form['funcionario_apellido2'],
+                 "cargo": serialized_form['funcionario_cargo']
+               },    
+
+               "dataset": {
+                 "descripcion": serialized_form['dataset_descripcion'],
+                 "campos": serialized_form['dataset_campos'],
+                 "fecha_inicial": "",
+                 "fecha_final": ""
+               },
+
+               "privacidad": {
+                "privacidad_vulnera_flag": "",
+                "privacidad_campos_vulnerables": ["", ""]
+               },
+
+               "clasificacion": {
+                  "datos_flag":  'clasificacion_datos_flag' in serialized_form,
+                  "antiguedad_mayor_a_15_anios_flag":  'clasificacion_antiguedad_mayor_a_15_anios_flag' in serialized_form
+               },
+
+               "restriccion": {
+                 "datos_industriales_flag":  'datos_industriales_flag' in serialized_form,
+                 "seguridad_nacional_flag":  'seguridad_nacional_flag' in serialized_form,
+                 "relaciones_internacionales_flag": "",
+                 "investigaciones_en_curso_flag":  'investigaciones_en_curso_flag' in serialized_form
+               }
+
+            }
+        except Exception as e:
+            print("AHHHHH")
+            print(e)
+    print("finished")
+    print(answers)
+    html = render_template('solicitud.html', solicitud=generate_base_letter(answers))
     return { 'html': html }
 
 @app.route('/', methods=['GET'])
